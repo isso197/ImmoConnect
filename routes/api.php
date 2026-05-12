c49102a7d95c3f7ebdef3db01e720bd094950ba8
Assia <assiahousn@gmail.com>
2026-05-12 07:05:20 +0100

    the authentification routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+//Auth Public
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class ,'login']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//private
+Route::middleware('auth:sanctum')->group(function () => {
+
+	Route::post('/logout' ,[AuthController::class , 'logout']);
+	Route::post('/logout-all',[AuthController::class ,'logoutFromAll']);
+});
