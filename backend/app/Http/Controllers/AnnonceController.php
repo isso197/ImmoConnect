b38e63da0bc935b053f6b22d2be21e6beb9c02a4
Assia <assiahousn@gmail.com>
2026-05-14 17:02:41 +0100

    Favori and Annonce Controllers

--- backend/app/Http/Controllers/AnnonceController.php
+++ backend/app/Http/Controllers/AnnonceController.php
@@ -3,8 +3,97 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Annonce;
 
-class AnnonceController
+class AnnonceController extends Controller
 {
-    //
+    public function index(){
+        return response()->json([
+            "status"=>"success",
+            "data"=>Annonce::all()
+        ]);
+    }
+
+    public function show(Annonce $annonce){
+        return response()->json([
+            "status"=>"success",
+            "data"=>$annonce
+        ]);
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            "title"=>"required|string|max:255",
+            "property_type"=>"required|in:appartement,maison,terrain",
+            "listing_type"=>"required|in:vente,location",
+            "price"=>"required|numeric",
+            "city"=>"required|string|max:255",
+            "address"=>"nullable|string|max:255",
+            "rooms"=>"nullable|numeric",
+            "bathrooms"=>"nullable|numeric"
+        ]);
+        $validated['user_id'] = $request->user()->id;
+
+        $annonce = Annonce::create($validated);
+
+        return response()->json([
+            "status"=>"success",
+            "data"=>$annonce
+        ]);
+    }
+
+    public function update(Request $request , Annonce $annonce){
+
+        //not any user could update
+        if($request->user()->id !== $annonce->user_id){
+            return response()->json([
+                "message"=>"Unauthorised"
+            ],403);
+        }
+
+        $validated = $request->validate([
+            "title"=>"sometimes|string|max:255",
+            "property_type"=>"sometimes|in:appartement,maison,terrain",
+            "listing_type"=>"sometimes|in:vente,location",
+            "price"=>"sometimes|numeric",
+            "city"=>"sometimes|string|max:255"
+            "address"=>"nullable|string|max:255",
+            "rooms"=>"nullable|numeric",
+            "bathrooms"=>"nullable|numeric"
+        ]);
+
+        $annonce->update($validated);
+
+        return response()->json([
+            "status"=>"success",
+            "data"=>$annonce
+        ]);
+    }
+
+    public function destroy(Annonce $annonce){
+
+        if($request->user()->id !== $annonce->user_id){
+            return response()->json([
+                "message"=>"Unauthorised"
+            ],403);
+        }
+
+        $annonce->delete();
+        return response()->json([
+            "success"=>true,
+            "message"=>"annonce well deleted"
+        ]);
+    }
+
+    public function latest(){
+        $annonces = Annonce::latest()->get();
+        return response()->json([
+            "success"=>true,
+            "data"=>$annonces
+        ]);
+
+    }
 }
+
+
+
--- backend/app/Http/Controllers/FavoriController.php
+++ backend/app/Http/Controllers/FavoriController.php
@@ -3,8 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Favori;
+use App\Models\Annonce;
 
 class FavoriController extends Controller
 {
-    //
+    public function index(Request $request){
+        $favoris = Favori::where("user_id",$request->user()->id);
+        return response()->json([
+            "success"=>true,
+            "message"=>"all fav annonce",
+            "data"=>$favoris
+        ]);
+    }
+
+    public function store(Request $request, Annonce $annonce){
+
+        $exists = Annonce::where('user_id',$request->user()->id)->where('annonce_id',  $annonce->id)->exists();
+
+        if($exists){
+            return response()->json([
+                "success"=>false,
+                "message"=>"the annonce is already in favorus"
+            ]);
+        }
+
+        $favori = Favori::create([
+        "annonce_id" => $annonce->id,
+        "user_id" => $request->user()->id
+        ]);
+
+        return response()->json([
+            "success"=>true,
+            "message"=>"all fav annonce",
+            "data"=>$favori
+        ]); 
+    }
+
+    public function delete(Request $request , Annonce $annonce){
+
+        $favori = Favori::where('user_id',$request->user()->id)->where("annonce_id",$annonce->id)->first();
+        if(!$favori){
+            return response()->json([
+                "success"=>false,
+                "message"=>"the annonce is not already deleted"
+            ]);
+        }
+
+        $favori->delete();
+        return response()->json([
+            "success"=>true,
+            "message"=>"the annonce is deleted"
+        ]);
+    }
 }
+
